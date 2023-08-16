75ed872447eda202042043d0679fdfee113df707
AngelTravieso <angeltraviesoc@gmail.com>
2023-08-16 14:46:18 -0400

    Agregado prefijo /api global

--- src/main/java/com/test/user/app/controllers/TutorialController.java
+++ src/main/java/com/test/user/app/controllers/TutorialController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v1")
 public class TutorialController {
 
     @Autowired
